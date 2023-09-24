35ee85d65df9c4b6a315ff8546668f1992662495
Nico Herlim <nicoherlim2003@gmail.com>
2023-09-24 10:49:34 +0700

    change String s -> theme for readability

--- lib/logic/display.dart
+++ lib/logic/display.dart
@@ -17,31 +17,40 @@ class DisplayEtc {
     _title = title;
   }
 
-  void dynamicAccentChanger(Duration currentTimerState, Duration freezedTimerState) {
+  void dynamicAccentChanger(
+      Duration currentTimerState, Duration freezedTimerState) {
     if (currentTimerState.inSeconds <= freezedTimerState.inSeconds * 0.2) {
       _currentAccent = Colors.red;
-    } else if (currentTimerState.inSeconds <= freezedTimerState.inSeconds * 0.3) {
+    } else if (currentTimerState.inSeconds <=
+        freezedTimerState.inSeconds * 0.3) {
       _currentAccent = Colors.deepOrange;
-    } else if (currentTimerState.inSeconds <= freezedTimerState.inSeconds * 0.4) {
+    } else if (currentTimerState.inSeconds <=
+        freezedTimerState.inSeconds * 0.4) {
       _currentAccent = Colors.orange;
-    } else if (currentTimerState.inSeconds <= freezedTimerState.inSeconds * 0.5) {
+    } else if (currentTimerState.inSeconds <=
+        freezedTimerState.inSeconds * 0.5) {
       _currentAccent = Colors.lightGreen;
-    } else if (currentTimerState.inSeconds <= freezedTimerState.inSeconds * 0.6) {
+    } else if (currentTimerState.inSeconds <=
+        freezedTimerState.inSeconds * 0.6) {
       _currentAccent = Colors.green;
-    } else if (currentTimerState.inSeconds <= freezedTimerState.inSeconds * 0.7) {
+    } else if (currentTimerState.inSeconds <=
+        freezedTimerState.inSeconds * 0.7) {
       _currentAccent = Colors.teal;
-    } else if (currentTimerState.inSeconds <= freezedTimerState.inSeconds * 0.8) {
+    } else if (currentTimerState.inSeconds <=
+        freezedTimerState.inSeconds * 0.8) {
       _currentAccent = Colors.cyan;
     } else {
       _currentAccent = Colors.lightBlue;
     }
   }
 
-  void changeThemeMode(String s) {
-    if (s == "Light") {
+  void changeThemeMode(String theme) {
+    if (theme == "Light") {
       _currentThemeMode = ThemeMode.light;
       return;
-    } else if (s == "Dark") {
+    }
+
+    if (theme == "Dark") {
       _currentThemeMode = ThemeMode.dark;
       return;
     }
