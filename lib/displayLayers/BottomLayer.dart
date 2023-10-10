c4e71ffcdea52836432800636199cdb4576f2753
bootloopmaster636 <christhartono@hotmail.com>
2023-10-10 15:56:00 +0700

    feat: add lottie bg

--- lib/displayLayers/BottomLayer.dart
+++ lib/displayLayers/BottomLayer.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lottie/lottie.dart';
 
 class BottomLayer extends ConsumerWidget {
   const BottomLayer({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Theme.of(context).colorScheme.secondaryContainer,
-            Theme.of(context).colorScheme.tertiaryContainer,
-          ],
+    return Stack(alignment: Alignment.center,
+        children: [
+          Lottie.asset(
+            'assets/lottie_pattern_bg.json',
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.cover,
+          ),
+      Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Theme.of(context).colorScheme.secondaryContainer.withOpacity(0.96),
+              Theme.of(context).colorScheme.tertiaryContainer.withOpacity(0.96),
+            ],
+          ),
         ),
       ),
-    );
+    ]);
   }
 }
