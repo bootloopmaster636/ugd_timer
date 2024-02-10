ae7213328422416e9f17d53f3bb7f3080f4d651f
bootloopmaster636 <christhartono@hotmail.com>
2024-02-10 15:12:15 +0700

    res: change bg resource

--- lib/screen/background.dart
+++ lib/screen/background.dart
@@ -1,4 +1,3 @@
-import 'package:css_filter/css_filter.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:lottie/lottie.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
@@ -17,21 +16,16 @@ class Background extends StatelessWidget {
       child: Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          const Acrylic(),
-          CSSFilter.apply(
-            value: CSSFilterMatrix()
-                .grayscale(1)
-                .brightness(FluentTheme.of(context).brightness == Brightness.light ? 8 : 1),
-            child: Lottie.asset(
-              'assets/lottie/bg-geometry.json',
-              repeat: true,
-              renderCache: RenderCache.drawingCommands,
-              fit: BoxFit.cover,
-              addRepaintBoundary: true,
-            ),
+          Lottie.asset(
+            'assets/lottie/bg-geo.json',
+            repeat: true,
+            addRepaintBoundary: true,
+            backgroundLoading: true,
+            renderCache: RenderCache.drawingCommands,
+            fit: BoxFit.cover,
           ),
           Container(
-            color: FluentTheme.of(context).accentColor.withOpacity(0.1),
+            color: FluentTheme.of(context).accentColor.withOpacity(0.2),
           ),
         ],
       ),
